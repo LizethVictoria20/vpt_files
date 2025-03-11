57c3e8ab6119aee9418ba0ff419aa7b51edf5fb5
LizethVictoria20 <41175343+LizethVictoria20@users.noreply.github.com>
2025-03-11 08:08:47 -0500

    update:creando login con user y email

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,3 +1,4 @@
+from operator import or_
 import os
 from io import BytesIO
 import dropbox
@@ -33,7 +34,7 @@ def login():
     if form.validate_on_submit():
         username = form.username.data
         password = form.password.data
-        user = User.query.filter_by(username=username).first()
+        user = User.query.filter(or_(User.username == username, User.email == username)).first()
         if user and bcrypt.check_password_hash(user.password_hash, password):
             login_user(user)
             is_admin = any(role.slug == 'admin' for role in user.roles)
